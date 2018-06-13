3478aede58a0aabacd3965f56dfd8df7424802b3
CiaraSparkFun <39280626+CiaraSparkFun@users.noreply.github.com>
2018-06-13 16:35:08 -0600

    lib ver roll
    expanded last example, fixed space in keywords

--- examples/Example11_AdvancedSettings/Example11_AdvancedSettings.ino
+++ examples/Example11_AdvancedSettings/Example11_AdvancedSettings.ino
@@ -14,14 +14,23 @@
 #include <Wire.h>
 #include "Qwiic_LED_Stick.h" // Click here to get the library: http://librarymanager/All#SparkFun_Qwiic_LED_Stick
 
-LED LEDStick; //Create an object of the LED class
+LED LEDStick1; //Create an object of the LED class
+LED LEDStick2; //Create an object of the LED class
+
 
 void setup() {
   Wire.begin();
   Serial.begin(9600);
-  //Start up communication with the LED Stick at address 0x29 
-  LEDStick.begin(0x29);
-  LEDStick.setLEDColor(10, 10, 10);
+  //Start up communication with the LED Stick at address 0x29
+  LEDStick1.begin(0x29);
+  //Set the whole 1st LED Stick dim white
+  LEDStick1.setLEDColor(10, 10, 10);
+  //Start up communication with another LED Stick at address 0x23
+  //Using the specified I2C port Wire
+  LEDStick2.begin(0x23, Wire);
+  //Set the whole 2nd LED Stick dim white
+  LEDStick2.setLEDColor(10, 10, 10);
+
 
 }
 
