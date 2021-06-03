2ad57f68d25d11f1b1b55973d1e3c67af2e6898c
pmakin-png <54557724+pmakin-png@users.noreply.github.com>
2021-06-03 13:03:58 -0600

    Fix examples 10 and 11 with updated serial baud rate

--- examples/Example10_ChangeAddress/Example10_ChangeAddress.ino
+++ examples/Example10_ChangeAddress/Example10_ChangeAddress.ino
@@ -18,21 +18,87 @@
 
 LED LEDStick; //Create an object of the LED class
 
+int currentAddress = 0x23;
+int newAddress = 0x29;
+
 void setup() {
   Wire.begin();
-  Serial.begin(9600);
+  Serial.begin(115200);
+  
   //Start up communication with the LED Stick
-  LEDStick.begin();
-  //Change the address of the LEDStick from 0x23 to 0x29
-  LEDStick.changeAddress(0x29);
-  //Set all LEDs dim white.
-  //Note that the call for this command doesn't depend on an address
-  LEDStick.setLEDColor(10, 10, 10);
+  if (LEDStick.begin(currentAddress) == false){
+    Serial.println("Qwiic LED Stick failed to begin. Please check wiring and try again!");
+  }
 
+  else {
+    Serial.println("Qwiic LED Stick ready!");
+    Serial.println();
+    
+    //Change the address of the LEDStick
+    //First, check that the address is valid
+    if (newAddress > 0x08 && newAddress < 0x77) {
+      Serial.println("Address is valid.");
+      Serial.print("Attempting to set the device address to 0x");
+      Serial.println(newAddress, HEX);
+      Serial.println();
+      
+      if (LEDStick.changeAddress(newAddress) == true){
+        Serial.println("I2C address change successful!");
+        Serial.println();
+      }
+      
+      //Set all LEDs dim white.
+      //Note that the call for this command doesn't depend on an address
+      LEDStick.setLEDColor(10, 10, 10);
+    } 
+    
+    else {
+      Serial.println("Address out of range! Try an address between 0x08 and 0x77");
+      Serial.println();
+    }
+    
+  }
 }
 
 void loop() {
+  //if no I2C device found or Qwiic button correctly set to new address, 
+  //scan for available I2C devices
+  byte error, address;
+  int nDevices;
 
+  Serial.println("Scanning...");
 
+  nDevices = 0;
+  for (address = 1; address < 127; address++ )
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.print(address, HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error == 4)
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.println(address, HEX);
+    }
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+
+  delay(5000);           // wait 5 seconds for next scan
 }
-
--- examples/Example11_AdvancedSettings/Example11_AdvancedSettings.ino
+++ examples/Example11_AdvancedSettings/Example11_AdvancedSettings.ino
@@ -8,8 +8,7 @@
   Feel like supporting our work? Buy a board from SparkFun!
   https://www.sparkfun.com/products/14783
 
-
-
+  This example shows how to use two LED Sticks on the same I2C bus
 */
 #include <Wire.h>
 #include "Qwiic_LED_Stick.h" // Click here to get the library: http://librarymanager/All#SparkFun_Qwiic_LED_Stick
@@ -20,14 +19,26 @@ LED LEDStick2; //Create an object of the LED class
 
 void setup() {
   Wire.begin();
-  Serial.begin(9600);
-  //Start up communication with the LED Stick at address 0x29
-  LEDStick1.begin(0x29);
+  Serial.begin(115200);
+  
+  //Start up communication with the LED Stick 1
+  if (LEDStick1.begin() == false){
+    Serial.println("Qwiic LED Stick 1 failed to begin. Please check wiring and try again!");
+    while(1);
+  }
+  
+  Serial.println("Qwiic LED Stick 1 ready!");
   //Set the whole 1st LED Stick dim white
   LEDStick1.setLEDColor(10, 10, 10);
-  //Start up communication with another LED Stick at address 0x23
-  //Using the specified I2C port Wire
-  LEDStick2.begin(0x23, Wire);
+  
+  //Start up communication with another LED Stick 2 at address 0x29
+  if (LEDStick2.begin(0x29) == false){
+    Serial.println("Qwiic LED Stick 2 failed to begin. Please check wiring and try again!");
+    while(1);
+  }
+
+  Serial.println("Qwiic LED Stick 2 ready!");
+
   //Set the whole 2nd LED Stick dim white
   LEDStick2.setLEDColor(10, 10, 10);
 
@@ -38,4 +49,3 @@ void loop() {
 
 
 }
-
